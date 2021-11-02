4a3913b8dfe5706354b0c3a60319947589942046
hyh <89124117+cxyhyh@users.noreply.github.com>
2021-11-02 17:34:25 +0800

    Add files via upload

---
+++ src/test/java/com/cloud/springcloudserver/SpringcloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.springcloudserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
